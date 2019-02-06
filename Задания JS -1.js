b546e080537c4637ea6b87a46114aba58c6c0a23
dongaev <42833984+dongaev@users.noreply.github.com>
2019-02-06 14:16:52 +0300

    commit

--- Задания JS -1.js
+++ Задания JS -1.js
@@ -322,3 +322,34 @@ switch (a) {
     default:
     alert(a + ' - это что-то непонятное!');
 }
+
+/* Переменная num может принимать 4 значения: 1, 2, 3 или 4. Если она имеет значение '1', то в переменную result запишем 'зима', если имеет значение '2' – 'весна' и так далее. Решите задачу через switch-case. */
+
+
+var num = 1;
+var result;
+
+switch(num){
+    case 1: 
+    result = 'Зима';
+    break;
+
+    case 2:
+    result = 'Весна';
+    break;
+
+    case 3:
+    result = 'Лето';
+    break;
+
+    case 4:
+    result = 'Осень';
+    break;
+
+    default:
+    alert(num + ' - это не месяц!')
+    break;
+
+}
+
+alert(result);
